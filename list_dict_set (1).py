c1bfdac852a4cadf08a3a6ecc0c9bca73d467ea8
SUJAI-S <sujai242004@gmail.com>
2024-06-21 14:44:38 +0530

    Add files via upload

---
+++ list_dict_set (1).py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#list
+no=[1,20,30]
+print(no)
+
+
+# In[2]:
+
+
+no.insert(1,10)
+print(no)
+
+
+# In[3]:
+
+
+no.append(40)
+print(no)
+
+
+# In[4]:
+
+
+no.remove(1)
+print(no)
+
+
+
+# In[5]:
+
+
+#dictionary
+dic={'sanjay':1,'sujai':2,'rahul':3,'ramesh':4}
+print(dic)
+
+
+# In[6]:
+
+
+dic1={'suresh':5,'raj':6}
+dic.update(dic1)
+print(dic)
+
+
+# In[9]:
+
+
+dic.pop('sanjay')
+print(dic)
+
+
+# In[12]:
+
+
+#set
+fruits={'apple','mango','orange'}
+print(fruits)
+added={'cherry','cucumber'}
+fruits.update(added)
+print(fruits)
+fruits.remove('mango')
+print(fruits)
+
+
+# In[ ]:
+
+
+
+
